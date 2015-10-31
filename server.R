ce00d9c70bd07b81be157918bacf3d10b134b200
rayhan1969 <rayhan@alumni.stanford.org>
2015-10-31 14:57:20 -0700

    R FIles

---
+++ server.R
@@ -0,0 +1,24 @@
+library(caret)
+library(shiny)
+library(UsingR)
+#code outside gets run only once outside of function
+CompHardware <- read.csv("w:/Ray/CompHardware.csv")
+FittedModel <- train(ERP ~ VendorName+ MYCT + MMIN + MMAX + CACH + CHMIN + CHMAX + PRP
+                     , method="glm", data=CompHardware)
+shinyServer(
+  function(input, output){ 
+    #code inside function but not in reactive run once for every user or page #refresh
+    #code in reactive function gets run repeatedly as needed when new values are #entered
+    output$calculated <- renderPrint({
+      input$goButton
+      VendorName = input$VendorName
+      MYCT = input$MYCT
+      MMIN = input$MMIN
+      MMAX = input$MMAX
+      CACH = input$CACH
+      CHMIN = input$CHMIN
+      CHMAX = input$CHMAX
+      PRP = input$PRP
+      predict(FittedModel,data.frame(VendorName, MYCT, MMIN, MMAX, CACH, CHMIN, CHMAX, PRP))})
+  }
+)
